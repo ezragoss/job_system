7477fae05161c1b12a30e60fb8d956eda496dd93
ezra goss <ezragoss@gmail.com>
2024-06-08 15:37:20 -0700

    Init commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <fmt/core.h>
+#include <glm/glm.hpp>
+
+#include <thread>
+
+int main() {
+    unsigned int available_cores = std::thread::hardware_concurrency();
+    std::cout << fmt::format("Available cores: {0}", available_cores) << std::endl;
+    return 0;
+}
